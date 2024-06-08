b240be7ae3df47f4904994cfb9c4d6b4823bce0d
ABISHEK-MITRAHSOFT <abishek.j@mitrahsoft.com>
2024-06-08 16:22:11 +0530

    Removed juno

--- src/root/App.jsx
+++ src/root/App.jsx
@@ -1,26 +1,16 @@
-import { initJuno } from "@junobuild/core";
-import React, { useEffect } from "react";
-import { Provider } from "react-redux";
+import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { WalletStandardProvider } from "@wallet-standard/react";
+import { PetraWallet } from "petra-plugin-wallet-adapter";
+import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import LoadingWrapper from "../component/loading-wrapper";
 import store, { persistor } from "../redux/store";
 import "./../App.css";
 import MainLayout from "./layout";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
-import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 
 const App = () => {
-  const satelliteId = process.env.REACT_APP_SATELLITE_ID;
-
-  useEffect(() => {
-    (async () =>
-      await initJuno({
-        satelliteId,
-      }))();
-  }, [satelliteId]);
-
   const wallets = [new PetraWallet()];
 
   return (
